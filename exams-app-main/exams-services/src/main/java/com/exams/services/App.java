342a37d4cbda27314e4f53c0dc2677c8904c93c0
Sushma92 <nuthanap1992@gmail.com>
2019-11-23 11:28:18 -0600

    Initail version of creating 5 projects

---
+++ exams-app-main/exams-services/src/main/java/com/exams/services/App.java
@@ -0,0 +1,13 @@
+package com.exams.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ exams-app-main/src/main/java/com/exams/main/App.java
@@ -0,0 +1,13 @@
+package com.exams.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
